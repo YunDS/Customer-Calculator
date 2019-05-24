e1be39684d45bd0b8de72cdd2ce707913b51356b
Yun <yds927848@naver.com>
2019-05-24 19:54:55 +0900

    my base projects

---
+++ Jproject/src/jproject/MlCalcController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jproject;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author yds92
+ */
+public class MlCalcController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
